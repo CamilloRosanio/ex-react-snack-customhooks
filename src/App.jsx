0291ca69d74b2bb31bf73ca1166e98abb910d54c
MyUsername <rosanio.c@gmail.com>
2025-03-24 12:41:40 +0100

    Snack 1 - versione base

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+// IMPORT CUSTOM-HOOK - useSwitch
+import useSwitch from "./useSwitch";
+
+
 function App() {
+
+  // Qui dichiaro all'interno del COMPONENT gli elementi del mio hook che voglio utilizzare.
+  const [isOn, toggle] = useSwitch();
 
   return (
     <>
-      App content here
+      <div>
+        {/* Nel titolo sto usando il VALUE reattivo del mio hook personalizzato */}
+        <h1>Il valore è: {isOn ? 'ON' : 'OFF'}</h1>
+
+        {/* Nel bottone invece sto passando la funzione "toggle" da utilizzare al click */}
+        <button onClick={toggle}>Cambia Stato</button>
+      </div>
     </>
   )
 }
---
+++ src/useSwitch.jsx
@@ -0,0 +1,46 @@
+// CUSTOM-HOOKS
+/*
+Per creare dei CUSTOM-HOOKS con REACT, è fondamentale che il componente si chiami"useNome".
+La creazione di CUSTOM-HOOKS è fondamentale per gestire, alleggerire e centralizzare il nostro codice
+qualora si necessitasse di logiche da ripetere e usare in più contesti, anche differenti tra loro.
+
+Proprio come i COMPONENTS, i CUSTOM-HOOKS devono essere dichiarati; in questo caso si parla di funzioni dichiarative.
+In seguito devono essere esportati.
+
+Importantissimo ricordare che i CUSTOM-HOOKS possono usare al loro interno gli HOOKS di REACT (useEffect, useState, etc..),
+e che la cosa più comoda è scriverne la logica in un file a parte come questo, per poi importarlo dove ci serve,
+proprio come un qualsiasi altro componente.
+
+Il vantaggio dei CUSTOM-HOOKS è che scroporo la logica dai COMPONENTS in cui vengono utilizzati.
+*/
+
+
+
+// UTILITY
+import { useState } from "react";
+
+
+
+// DICHIARAZIONE CUSTOM-HOOK
+
+/* Un parametro che si può passare è il valore iniziale (o di DEFAULT qualora manchi quello iniziale).
+Per applicare un DEFAULT basta mettere "parametro = valore di default". In questo caso sto usando FALSE come DEFAULT.*/
+export default function useSwitch(initialValue = false) {
+
+    const [isOn, setIsOn] = useState(initialValue)
+
+    const toggle = () => {
+        // Ricordiamoci che i SET di useState accettano come argomento il valore corrente (qui chiamato "curr").
+        // In questo caso, trattandosi di un BOOLEANO, imposto tramite toggle il valore opposto a quello corrente.  
+        setIsOn(curr => !curr)
+    }
+
+    // Nel RETURN specifico l'array di elementi di questo CUSTOM-HOOK che voglio restituire.
+    // Li potrò usare nel componente in cui li importo tramite DESTRUCTURING se voglio.
+    return [isOn, toggle]
+
+}
+
+// Per vederne l'utilizzo, andare su App.jsx
+
+
